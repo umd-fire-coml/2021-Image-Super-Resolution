b37494e427875b2dbbb2b1c955cf89030f6f4876
jiangclumd <jiangcl@terpmail.umd.edu>
2021-09-22 17:05:55 -0400

    Update verify.py
    Updated the subsets of the dataset path based on the datasets downloaded.

--- verify.py
+++ verify.py
@@ -4,11 +4,12 @@ from os import path
 def check(verbose):
    # Check if DIV2K is a complete dataset.
    if verbose == 1:
-       print('Checking dataset...')
-   dataset_path = 'DIV2K/LR'
+        print('Checking dataset...')
+   dataset_path = 'DIV2K'
    if path.exists(dataset_path):
        # Check if .. contains all required LR subsets.
-       subsets_LR = []
+       subsets_LR = ["DIV2K_train_LR_bicubic","DIV2K_train_LR_unknown", 
+       "DIV2K_valid_LR_bicubic", "DIV2K_valid_LR_unknown"]
        for subset in subsets_LR:
            subset_path = dataset_path + '/' + subset
            if path.exists(subset_path):
@@ -21,10 +22,19 @@ def check(verbose):
            else:
                print(subset_path + ' is MISSING!')
        # Check if DIV2K contains all required HR subsets.
-       subsets_HR = []
+       subsets_HR = ["DIV_train_LR_x8", "DIV2K_train_LR_mild", "DIV2k_train_LR_difficult", 
+       "DIV2K_train_LR_wild", "DIV2K_valid_LR_difficult", "DIV2K_valid_LR_wild", 
+       "DIV2K_train_HR", "DIV2K_valid_HR"]
        for subset in subsets_HR:
            subset_path = dataset_path + '/' + subset
            if path.exists(subset_path) == False:
                print(subset_path + ' is MISSING!')
    else:
        print('DIV2K is MISSING!')
+
+# for testing purposes
+# def main():
+    # check(1)
+
+# if __name__ == "__main__":
+    # ain()
