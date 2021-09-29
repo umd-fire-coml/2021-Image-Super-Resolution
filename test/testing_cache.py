46b362d73192ebfb2e406ce76862718faf5f2fec
Harpreet Multani <Harpreetmultani3636@gmail.com>
2021-09-29 08:56:57 -0400

    testing cache again

--- test/testing_cache.py
+++ test/testing_cache.py
@@ -7,4 +7,3 @@ def testing_cache():
     LR = arr[1]
     assert(LR[3] == 'src/DIV2K/DIV2K_train_LR_bicubic/X2/0004x2.png')
     assert(HR[5] == 'src/DIV2K/DIV2K_train_HR/0006.png')
-    print("works")
