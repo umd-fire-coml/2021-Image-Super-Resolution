c280e4a2a2fddd901f5afdfe34d55f1be8aad6ec
Ian H Roberts <irobert4@terpmail.umd.edu>
2021-10-01 10:13:20 -0400

    v2 model

---
+++ src/backbone.py
@@ -0,0 +1,28 @@
+#based on code from https://keras.io/examples/vision/super_resolution_sub_pixel/
+import tensorflow as tf
+
+import os
+import math
+import numpy as np
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import array_to_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+def get_model(upscale_factor=3, channels=1):
+    conv_args = {
+        "activation": "relu",
+        "kernel_initializer": "Orthogonal",
+        "padding": "same",
+    }
+    inputs = keras.Input(shape=(None, None, channels))
+    x = layers.Conv2D(64, 5, **conv_args)(inputs)
+    x = layers.Conv2D(64, 3, **conv_args)(x)
+    x = layers.Conv2D(32, 3, **conv_args)(x)
+    x = layers.Conv2D(channels * (upscale_factor ** 2), 3, **conv_args)(x)
+    outputs = tf.nn.depth_to_space(x, upscale_factor)
+
+    return keras.Model(inputs, outputs)
