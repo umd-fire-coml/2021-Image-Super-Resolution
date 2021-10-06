913ea3a696a62d6eb19c3f9755b6a85392526c60
Ian H Roberts <irobert4@terpmail.umd.edu>
2021-10-05 20:07:56 -0400

    Added test, made sure it works.

--- test/src/backbone.py
+++ test/src/backbone.py
@@ -1,24 +1,24 @@
 #based on code from https://keras.io/examples/vision/super_resolution_sub_pixel/
 import tensorflow as tf
-
 import os
 import math
 import numpy as np
-
 from tensorflow import keras
 from tensorflow.keras import layers
 
-def get_model(upscale_factor=3, channels=1):
+def get_model(upscale_factor=2, channels=3):
     conv_args = {
         "activation": "relu",
         "kernel_initializer": "Orthogonal",
         "padding": "same",
     }
     inputs = keras.Input(shape=(None, None, channels))
+    #A series of convolutional layers which pull out features of the image.
     x = layers.Conv2D(64, 5, **conv_args)(inputs)
     x = layers.Conv2D(64, 3, **conv_args)(x)
     x = layers.Conv2D(32, 3, **conv_args)(x)
     x = layers.Conv2D(channels * (upscale_factor ** 2), 3, **conv_args)(x)
+    #Turns the depth data in the convolutional layers into spacial data in the image.
     outputs = tf.nn.depth_to_space(x, upscale_factor)
 
     return keras.Model(inputs, outputs)
