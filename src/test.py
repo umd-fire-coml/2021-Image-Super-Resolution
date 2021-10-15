6dd436b883c6eee42c3257642973496de6959eba
Harpreet Multani <Harpreetmultani3636@gmail.com>
2021-10-15 09:58:51 -0400

    Update test.py

--- src/test.py
+++ src/test.py
@@ -5,8 +5,9 @@ from cache_creation import cache_creation
 #Generate PSNR, SSIM, etc.
 
 # Do we want a total count
-#total_bicubic_psnr = 0.0
-#total_test_psnr = 0.0
+
+total_bicubic_psnr = 0.0
+total_test_psnr = 0.0
 
 def testing():
     weights_path = ""
@@ -14,9 +15,20 @@ def testing():
     HR_cache = arr_cache[0]
     LR_cache = arr.cache[1]
     
+        
+    #compile the model beforehand
+    
+    model.load_weights(weights_path)
+    
+    #using arr_cache, iterate over eachimage, inputting it into model.predict
+    #calculate psnr and print it, print out info about the image
+    #model.predict
+    
     #calculating the pnsr with respect to the low-res image and predicted image
     bicubic_psnr = tf.image.psnr(lowres_img_arr, highres_img_arr, max_val=255)
     test_psnr = tf.image.psnr(predict_img_arr, highres_img_arr, max_val=255)
     
-    model.load_weights(weights_path)
+    #computing totals
+    total_bicubic_psnr += bicubic_psnr
+    total_test_psnr += test_psnr
     
