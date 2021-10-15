4ef11e6f0301c834abc8ad72b4d69233cd52df28
jiangclumd <jiangcl@terpmail.umd.edu>
2021-10-14 20:39:52 -0400

    Created test for generator

--- src/generator.py
+++ src/generator.py
@@ -1,22 +1,24 @@
 from sys import maxsize
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.utils import Sequence
 
-from cache_creation import cache_creation
+
+from tensorflow.keras.utils import Sequence
+from src.cache_creation import cache_creation
+
 
 class DataGenerator(Sequence):
     '''this is a random data generator, edit this data generator to read data from dataset folder and return a batch with __getitem__'''
 
-    def __init__(self, scale, batch_size=8, type = "train", n_dataset_items=800):
+    def __init__(self, scale, batch_size=8, type = "train", n_dataset_items=800, shuffle = False):
         self.scale = scale
         self.batch_size = batch_size
         self.n_dataset_items = n_dataset_items
         self.indexes = np.arange(self.n_dataset_items)
         self.images = cache_creation.cache_method(type, scale)
-        self.LR_imgs = self.images[0]
-        self.HR_imgs = self.images[1]
+        self.LR_imgs = self.images[1]
+        self.HR_imgs = self.images[0]
+        self.shuffle = shuffle
         self.on_epoch_end()
         
         
@@ -30,26 +32,35 @@ class DataGenerator(Sequence):
     def __get_input(self, index):
         LR = []
         HR = []
+        pair = []
         min_h_LR = maxsize
         min_w_LR = maxsize
         for i in index:
             LR_img = tf.keras.preprocessing.image.load_img(self.LR_imgs[i])
-            LR.append(LR_img)
-
             HR_img = tf.keras.preprocessing.image.load_img(self.HR_imgs[i])
+            LR_img = np.asarray(LR_img)
+            HR_img = np.asarray(HR_img)
+
+
+            LR.append(LR_img)
             HR.append(HR_img)
+
+            pair.append((LR_img, HR_img))
 
             min_h_LR = min(min_h_LR,LR_img.shape[0])
             min_w_LR = min(min_w_LR,LR_img.shape[1])
-        
+
         min_h_HR = self.scale * min_h_LR
         min_w_HR = self.scale * min_w_LR
 
-        for i in range (0, len(LR)):
-            LR[i] = self.crop_img(LR[i], min_w_LR, min_h_LR)
-            HR[i] = self.crop_img(HR[i], min_w_HR, min_h_HR)
+        #...
+        for i in range (0, len(pair)):
+            temp = list(pair[i])
+            temp[0] = self.crop_img(LR[i], min_w_LR, min_h_LR)
+            temp[1] = self.crop_img(HR[i], min_w_HR, min_h_HR)
+            pair[i] = tuple(temp)
             
-        return LR, HR
+        return pair
     
 
     def __getitem__(self, index):
@@ -59,25 +70,22 @@ class DataGenerator(Sequence):
         """
     
         # Generate indexes of the batch
-        batches = self.indexes[index * self.batch_size : (index + 1) * self.batch_size]
+        batches = self.indexes[(index-1) * self.batch_size : index * self.batch_size]
         
         # Generate data
-        LR, HR = self.__get_input(batches)       
+        X = self.__get_input(batches)       
 
 
-        # for loop batch
-        # ...
         # return a batch of HR and HR images 
-        return LR, HR
+        return X
 
     def crop_img(self, img, min_height, min_width):
         crop_img = img[0:min_height, 0:min_width, ...]
         return crop_img
 
-
-
-
     def on_epoch_end(self):
-        """Shuffle indexes after each epoch
+        """Shuffle indexes after each epoch if shuffle is True
         """
-        np.random.shuffle(self.indexes)
+        self.indexes = np.arange(self.n_dataset_items)
+        if self.shuffle == True:
+          np.random.shuffle(self.indexes)
