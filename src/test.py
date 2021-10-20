3d597d4966550a6591914716afe19fb2591aab89
Harpreet Multani <Harpreetmultani3636@gmail.com>
2021-10-20 11:48:50 -0400

    test script completed

--- src/test.py
+++ src/test.py
@@ -1,34 +1,24 @@
 from cache_creation import cache_creation
-#Load the model
-#SR test images
-#   DIV2k test
-#Generate PSNR, SSIM, etc.
+from tensorflow.keras.preprocessing.image import load_img
+import backbone
 
-# Do we want a total count
+def testing(weights):
+    weights_path = 'model/weights/' + weights
 
-total_bicubic_psnr = 0.0
-total_test_psnr = 0.0
-
-def testing():
-    weights_path = ""
-    arr_cache = cache_creation.cache_method("valid", 2)
-    HR_cache = arr_cache[0]
-    LR_cache = arr.cache[1]
-    
-        
-    #compile the model beforehand
-    
+    model = backbone.get_model(upscale_factor=2)
+    opt = tf.keras.optimizers.Adam(learning_rate=0.001)
+    model.compile(optimizer=opt, loss='mse', metrics=[tf.image.psnr])
+    testing_generator = DataGenerator(scale = 2,batch_size = 1, type = 'valid', n_dataset_items=100, shuffle = False)
     model.load_weights(weights_path)
     
-    #using arr_cache, iterate over eachimage, inputting it into model.predict
-    #calculate psnr and print it, print out info about the image
-    #model.predict
-    
-    #calculating the pnsr with respect to the low-res image and predicted image
-    bicubic_psnr = tf.image.psnr(lowres_img_arr, highres_img_arr, max_val=255)
-    test_psnr = tf.image.psnr(predict_img_arr, highres_img_arr, max_val=255)
-    
-    #computing totals
-    total_bicubic_psnr += bicubic_psnr
-    total_test_psnr += test_psnr
-    
+    total_test_psnr = 0
+    for i in range (801, 901):
+        lr, hr = testing_generator.__getitem__i
+        lr = np.squeeze(lr)
+        hr = np.squeeze(hr)
+        sr = model.predict(lr)
+        test_psnr = tf.image.psnr(sr, hr, max_val=255)
+        total_test_psnr += test_psnr
+        print("Image: " + str(i) + "PSNR: " + str(test_psnr))
+        
+    print("Average PSNR: " + str(total_test_psnr/100))
