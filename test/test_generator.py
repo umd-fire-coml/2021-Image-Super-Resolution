d36e60e9f0a5971e0975aaafd2e1054d9967440a
Allen Tu <atu1@umd.edu>
2021-12-10 10:38:21 -0500

    Update test_generator.py

--- test/test_generator.py
+++ test/test_generator.py
@@ -5,5 +5,5 @@ def test_generator():
     
     HR = a.HR_imgs
     LR = a.LR_imgs
-    assert(LR[5] == 'src/DIV2K/DIV2K_train_LR_bicubic/X2/0006x2.png')
-    assert(HR[1] == 'src/DIV2K/DIV2K_train_HR/0002.png')
+    assert(LR[5] == '../DIV2K/DIV2K_train_LR_bicubic/X2/0006x2.png')
+    assert(HR[1] == '../DIV2K/DIV2K_train_HR/0002.png')
